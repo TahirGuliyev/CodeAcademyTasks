1c812d2f07d89a249deb2661061fb6e33bd48399
Tahir Guliyev <53049809+TahirGuliyev@users.noreply.github.com>
2022-08-24 22:39:40 +0400

    Create 24.08.2022.cs

---
+++ ConsoleApp3/24.08.2022.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+internal class Program
+{
+	public static void Main(string[] args)
+	{
+        #region TASK1
+        //IEnumerable<int> numbers = Enumerable.Range(1, 100);
+        //foreach (var item in numbers)
+        //{
+        //	Console.WriteLine($"Neqativ: {item}, pozitiv: {item * (-1)}");
+        //} 
+        #endregion
+
+        #region TASK2
+        //Console.WriteLine("String daxil edin: ");
+        //string s = Console.ReadLine();
+        //Console.WriteLine(Reverse(s)); 
+        #endregion
+
+        #region TASK3
+        IEnumerable<int> numbers = Enumerable.Range(10, 20);
+		foreach (var item in numbers)
+		{
+			AsalSayi(item);
+		}
+        #endregion
+    }
+
+	#region TASK2
+	private static string Reverse(string s)
+	{
+		char[] charArray = s.ToCharArray();
+		Array.Reverse(charArray);
+		return new string(charArray);
+	}
+	#endregion
+	#region TASK3
+	private static bool AsalSayi(int number)
+	{
+		int i;
+		for (i = 2; i <= number - 1; i++)
+		{
+			if (number % i == 0)
+			{
+				return false;
+			}
+		}
+		if (i == number)
+		{
+			return true;
+		}
+		return false;
+	} 
+	#endregion
+}
