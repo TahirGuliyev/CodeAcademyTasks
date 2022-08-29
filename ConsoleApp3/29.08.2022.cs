5bef4e1875f1ba8f3431fe535cd0def48824e7cf
Tahir Guliyev <53049809+TahirGuliyev@users.noreply.github.com>
2022-08-29 19:30:47 +0400

    Create 29.08.2022.cs

---
+++ ConsoleApp3/29.08.2022.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<string> list = new List<string>()
+            {
+                "https://medium.com/",
+                "https://buttigieg.medium.com/one-year-in-parenting-has-taught-us-about-vulnerability-and-gratitude-170f6e94cbad"
+
+            };
+            foreach (string s in list)
+            {
+                Stopwatch st = new Stopwatch();
+                st.Start();
+
+                var client = new HttpClient();
+                var content = await client.GetStringAsync(s);
+                st.Stop();
+                Console.WriteLine(content.Length + " " + st.ElapsedMilliseconds);
+
+
+            }
+        }
+        
+    }
+
+}
