f9d1a3a3ea25683a4368f66b2870410c9753dd22
Tahir Guliyev <53049809+TahirGuliyev@users.noreply.github.com>
2022-09-01 22:54:10 +0400

    01.09.2022

---
+++ ConsoleApp3/01.09.2022/DelegateHomework/Company.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomework
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public static List<User> users = new List<User>();
+        public delegate void UserDelegate(User user);
+        public void CreateEmail(User user)
+        {
+            user.Email = user.Name + "." + user.Surname + "@gmail.com";
+        }
+        public void CreateUsername(User user)
+        {
+            user.Username = user.Name + "." + user.Surname;
+        }
+        public void Register(string name, string surname, string password) {
+            User user = new User();
+            user.Name = name;
+            user.Surname = surname;
+            user.Password = password;
+            UserDelegate userDelegate = new UserDelegate(CreateEmail);
+            userDelegate += CreateUsername;
+            userDelegate.Invoke(user);
+            Console.WriteLine("User elave olundu.");
+        }
+        public void GetAll()
+        {
+            foreach (var item in users)
+            {
+                Console.WriteLine(item.Id+" "+item.Name+" "+item.Surname+" "+item.Username);
+            }
+        }
+        public void DeleteById(int id)
+        {
+            users.Remove(GetById(id));
+        }
+        public void UpdateteById(int id)
+        {
+
+        }
+
+        public static User GetById(int id)
+        {
+            return users.Find(x => x.Id.Equals(id));
+        }
+        public static bool Login(string username, string password)
+        {
+            var usr = users.Find(x => x.Username.Equals(username));
+            if (usr.Username == username&&usr.Password == password)
+            {
+                Console.WriteLine("Login olundu!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Daxil etdiyiniz username ve ya password yanlisdir!");
+                return false;
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/01.09.2022/DelegateHomework/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace DelegateHomework
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool exit = true;
+            Console.WriteLine("Create a company:\nPlease enter company name");
+            Company company = new Company();
+            company.Name = Console.ReadLine();
+            while (exit)
+            {
+                Console.WriteLine("Please enter number:");
+                Console.WriteLine("1.Register a user(to company)\n" +
+                    "2. Login in a user (to company)\n" +
+                    "3. See all users in Company (GetAll)\n" +
+                    "4. Get one User from Company (GetById)\n" +
+                    "5. Update User's datas (UpdateById)\n" +
+                    "6. Delete User from Company (DeleteById)\n" +
+                    "7. Exit");
+                int numb = int.Parse(Console.ReadLine());
+                switch (numb)
+                {
+                    case 1:
+                        string regName, regSurname, regPassword;
+                        Console.WriteLine("Please enter name:");
+                        regName = Console.ReadLine();
+                        Console.WriteLine("Please enter surname:");
+                        regSurname = Console.ReadLine();
+                        Console.WriteLine("Please enter password");
+                        regPassword = Console.ReadLine();
+                        company.Register(regName, regSurname, regPassword);
+                        break;
+                    case 2:
+                        Console.WriteLine("Login\nPlease enter username:");
+                        string usrname = Console.ReadLine();
+                        Console.WriteLine("Please enter password");
+                        string password = Console.ReadLine();
+                        Company.Login(usrname, password);
+                        break;
+                    case 3:
+                        company.GetAll();
+                        break;
+                    case 4:
+                        Console.WriteLine("Id daxil edin");
+                        int id = int.Parse(Console.ReadLine());
+                        Company.GetById(id);
+                        break;
+                    case 5:
+                        Console.WriteLine("Id daxil edin");
+                        int id2 = int.Parse(Console.ReadLine());
+                        Company.UpdateById(id2);
+                    case 6:
+                        Console.WriteLine("Id daxil edin");
+                        int id1 = int.Parse(Console.ReadLine());
+                        company.DeleteById(id1);
+                    case 7:
+                        exit = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please enter a correct number!");
+                        continue;
+                }
+            }
+        }
+    }
+}
