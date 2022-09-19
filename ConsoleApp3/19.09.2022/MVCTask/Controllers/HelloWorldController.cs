a5a2e73ddcbce7756d53e0411c338170f0090d5f
Tahir Guliyev <53049809+TahirGuliyev@users.noreply.github.com>
2022-09-19 21:02:54 +0400

    19.09.2022

---
+++ ConsoleApp3/19.09.2022/MVCTask/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MVCTask.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
